b1b73522240f0afcbb66c590555bc671779dfbfd
roobottom <roo@roodesign.co.uk>
2023-06-05 23:11:02 +0100

    adding staticCMS

--- eleventy.config.js
+++ eleventy.config.js
@@ -19,6 +19,7 @@ module.exports = function (eleventyConfig) {
 
   // Passthrough
   eleventyConfig.addPassthroughCopy({ './app/images': '.' })
+  eleventyConfig.addPassthroughCopy({ './admin': './admin' })
 
   // Config
   return {
